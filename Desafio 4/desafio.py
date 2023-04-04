406237ccae09c6cbe0e762386b9e39d6ba3ec304
isadoraeduarda <isaesantos7@gmail.com>
2023-04-04 16:10:50 -0300

    exercicios realizado

---
+++ Desafio 4/desafio.py
@@ -0,0 +1,12 @@
+n = int (input())
+
+for i in range(1, n+1):
+    n1 = i**1
+    n2 = i**2
+    n3 = i**3
+    n4 = n1
+    n5= n2 + 1
+    n6 = n3 + 1
+
+    print("{} {} {}".format(n1, n2, n3))
+    print ("{} {} {}".format(n4, n5, n6))
